94f21a5149cc53c9f6a4779ff0229e6da6325a15
Kyle Eggleston <kyle@dreamsense.com>
2018-06-15 16:28:11 -0600

    Can't forget Saturday!

--- perl/days.pl
+++ perl/days.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday);
+my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
 
 for my $day (@days) {
   print $day . "\n";
