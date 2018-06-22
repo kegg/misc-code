de6ec302e6bb8e05632e2c1b8070af7c60f3c4ce
Kyle Eggleston <kyle@dreamsense.com>
2018-06-22 08:47:49 -0600

    Added package-info.java

--- java/HelloWorld/src/com/askearly/life/HelloWorld.java
+++ java/HelloWorld/src/com/askearly/life/HelloWorld.java
@@ -9,6 +9,9 @@ package com.askearly.life;
  * To live a life that is full of pain, is not something we all wish to do at
  * times, I know that's not what I'd want in a life. So we start with a simple
  * Hello.
+ * <p>
+ * <strong>Params:</strong><br>
+ * None
  * @author Kyle Eggleston
  * @author Copyright (c) 2018
  * @version 1.0 
---
+++ java/HelloWorld/src/com/askearly/life/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <strong>Description:</strong><br>
+ * Welcome to life. This is simply a test of javadocs...nothing important is
+ * going on in these classes. You can ignore all of this if you wish...I mean I
+ * sure would. Think of it is a testbed, it's not a proof of concept, as a proof
+ * of concept was created long before javadoc became an integrated piece of the
+ * Java programming language.
+ *
+ * @author Kyle Eggleston
+ * @author Copyright (c) 2018
+ * @version 1.0
+ */
+package com.askearly.life;
