4d4eb62b2147a0549702de677f713cdb9fdae23b
Kyle Eggleston <kyle@dreamsense.com>
2018-06-22 08:29:42 -0600

    Added TheBeginning

--- java/HelloWorld/src/com/askearly/life/HelloWorld.java
+++ java/HelloWorld/src/com/askearly/life/HelloWorld.java
@@ -1,9 +1,24 @@
 package com.askearly.life;
 
+/**
+ * <strong>Description:</strong><br>
+ * Life is weird at times. There's not much to be said about it, this much is
+ * true, yet here we are doing all we can to have that life. To live such a life
+ * that is fruitful and possible.
+ * <p>
+ * To live a life that is full of pain, is not something we all wish to do at
+ * times, I know that's not what I'd want in a life. So we start with a simple
+ * Hello.
+ * @author Kyle Eggleston
+ * @author Copyright (c) 2018
+ * @version 1.0 
+*/
+
 public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    new TheBeginning();
   }
 
 }
---
+++ java/HelloWorld/src/com/askearly/life/TheBeginning.java
@@ -0,0 +1,19 @@
+package com.askearly.life;
+
+/**
+ * <strong>Description:</strong><br>
+ * The beginning is a bit different for everyone. Not everything has a
+ * determined time or place in this life. It is but a life where nothing can
+ * come without a price.
+ * @author Kyle Eggleston
+ * @author Copyright (c) 2018
+ * @version 1.0
+*/
+
+public class TheBeginning {
+
+  public TheBeginning() {
+    System.out.println("This is only the beginning of things to come.");
+  }
+
+}
