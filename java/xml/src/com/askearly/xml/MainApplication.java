d5d2c9e92ced4e8cca569604fe3aaae6ccbc4812
Kyle Eggleston <kyle@dreamsense.com>
2018-12-19 13:45:28 -0700

    Add files via upload

---
+++ java/xml/src/com/askearly/xml/MainApplication.java
@@ -0,0 +1,9 @@
+package com.askearly.xml;
+
+public class MainApplication {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ java/xml/src/com/askearly/xml/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <strong>Description:</strong><br>
+ * This is a set of xml utilities that I've written.
+ *
+ * @author Kyle Eggleston
+ * @author Copyright (c) 2018
+ * @version 1.0
+ */
+package com.askearly.xml;
