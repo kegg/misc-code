a5a456c95b041e8623cf3977688f6607e33c4d61
Kyle Eggleston <kyle@dreamsense.com>
2019-05-24 10:24:52 -0600

    Added text input sample

--- java/text/src/dreamsense/main/TextAdventure.java
+++ java/text/src/dreamsense/main/TextAdventure.java
@@ -1,14 +1,43 @@
 package com.dreamsense.main;
 
+import java.util.Scanner;
+
 public class TextAdventure {
 
   public TextAdventure() {
 
   }
 
-  public static void main(String[] args) {
+  public void init() {
     Utils.clear();
+  }
+
+  public void run() {
+    String s;
+    boolean done = false;
+
+    Scanner input = new Scanner(System.in);
+
+    while (!done) {
+      System.out.print("Command: ");
+      s = input.next();
+
+      if (s.equals(".quit")) {
+        done = true;
+      } else {
+        process(s);
+      }
+    }
+  }
+
+  public void process(String s) {
+    System.out.println(s);
+  }
+
+  public static void main(String[] args) {
     TextAdventure ta = new TextAdventure();
+    ta.init();
+    ta.run();
   }
 
 }
