cfea791dc53435b53f6d2a564d2bb9ce19218505
Kyle Eggleston <kyle@dreamsense.com>
2019-05-24 10:05:54 -0600

    added constructor

--- java/text/src/dreamsense/main/TextAdventure.java
+++ java/text/src/dreamsense/main/TextAdventure.java
@@ -2,8 +2,12 @@ package com.dreamsense.main;
 
 public class TextAdventure {
 
+  public TextAdventure() {
+
+  }
+
   public static void main(String[] args) {
-    System.out.println("Welcome to Text Adventure.");
+    TextAdventure ta = new TextAdventure();
   }
 
 }
