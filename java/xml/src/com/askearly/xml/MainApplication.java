b051c4f7afadb7133df8744891205c455d791227
Kyle Eggleston <kyle@dreamsense.com>
2023-11-12 16:27:11 -0700

    Changed Xml to display Color

--- java/xml/src/com/askearly/xml/MainApplication.java
+++ java/xml/src/com/askearly/xml/MainApplication.java
@@ -1,9 +1,44 @@
+/**
+ * We're actually going to play with colors here, not xml
+ */
 package com.askearly.xml;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+/**
+ * We're actually going to play with colors here, not xml
+ */
 public class MainApplication {
 
-  public static void main(String[] args) {
+  /**
+   * This needed to be here because of reasons, so there.
+   */
+  public MainApplication() {
 
+  }
+
+  /**
+ * We're actually going to play with colors here, not xml
+ * @param args The program args, takes 3 arguments integers
+ */
+  public static void main(String[] args) {
+      if (args.length == 0) {
+        args = new String[]{"255", "0", "0"};
+      }
+      Color color = new Color(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+      JDialog dialog = new JDialog();
+      dialog.setTitle("Color Test");
+      dialog.setSize(new Dimension(300,300));
+      dialog.setLocationRelativeTo(null);
+      dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+      JPanel panel = new JPanel();
+      panel.setBackground(color);
+      dialog.add(panel);
+      dialog.setVisible(true);
   }
 
 }
