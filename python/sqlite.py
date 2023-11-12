b33dd984e4a0ae900f3ed56c82abe587e66d1e19
Kyle Eggleston <kyle@dreamsense.com>
2023-11-12 13:20:27 -0700

    added deleteion of file to sqlite

--- python/sqlite.py
+++ python/sqlite.py
@@ -1,15 +1,21 @@
+import os
 import sqlite3
-conn = sqlite3.connect('life.db')
+conn = sqlite3.connect('life.db');
 
-c = conn.cursor()
+c = conn.cursor();
 
-c.execute("CREATE TABLE words (id integer primary key, note text)")
+c.execute("CREATE TABLE words (id integer primary key, note text)");
 
-c.execute("INSERT INTO words (note) VALUES ('Hi, Hello There!')")
+c.execute("INSERT INTO words (note) VALUES ('Hi, Hello There!')");
 
-conn.commit()
+conn.commit();
 
 for row in c.execute('SELECT * FROM words'):
-  print row
+  print(row);
 
-conn.close()
+conn.close();
+
+if os.path.exists('life.db'):
+  os.remove('life.db');
+else:
+  print('The file does not exist');
