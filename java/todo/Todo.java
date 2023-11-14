b97664bd901e427eb85a751357f31895da907b6a
Kyle Eggleston <kyle@dreamsense.com>
2023-11-13 20:18:32 -0700

    Added lots of stuffs

---
+++ java/todo/Todo.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+
+public class Todo {
+
+    public static void main(String[] args) {
+        Console console = System.console();
+        
+        Todo todo = new Todo();
+        todo.showMenu();
+    }
+
+    private void showMenu() {
+        
+    }
+}
