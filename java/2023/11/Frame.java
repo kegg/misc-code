32733bdb834226e2f916750fbaf1ead2c1e6b11b
Kyle Eggleston <kyle@dreamsense.com>
2023-11-30 15:28:10 -0700

    Updated Frame format, margin etc

--- java/2023/11/Frame.java
+++ java/2023/11/Frame.java
@@ -5,11 +5,17 @@ public class Frame extends JFrame {
     public static void main(String[] args) {
         Frame frame = new Frame();
         frame.setTitle("Hello World!");
-        frame.add(new JLabel("Hello World! Here is some text to make the frame rather long..."), BorderLayout.CENTER);
+
+        JPanel helloPanel = new JPanel();
+        JLabel hello = new JLabel("Hello World! Here is some text to make the frame a little long to demonstrate...");
+        helloPanel.add(hello);
+        frame.add(helloPanel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
         buttonPanel.add(new JButton("OK"));
+        
         JButton closeButton = new JButton("Close");
         closeButton.addActionListener(e->frame.dispose());
         buttonPanel.add(closeButton);
