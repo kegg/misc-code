6095a6e7988bdfeddb8bc9bcb67c604bc89ead37
Kyle Eggleston <kyle@dreamsense.com>
2023-12-05 21:41:50 -0700

    corrected syntax

--- perl/test.pl
+++ perl/test.pl
@@ -1,14 +1,13 @@
 #!/usr/bin/perl
 use strict;
-use warnings;
 use Text::Template;
 
-$template = Text::Template->new(
+my $template = Text::Template->new(
     type=>'file',
     source=>'file.tmpl'
 );
 
 $Q::name = 'Fred';
 $Q::age = 45;
-$text = $template->fill_in(package=>'Q');
+my $text = $template->fill_in(package=>'Q');
 print $text;
