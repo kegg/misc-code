0e6134e36fcb020a5b55f6254bd0213abd1bc886
Kyle Eggleston <kyle@dreamsense.com>
2023-12-05 21:31:48 -0700

    added some programs

---
+++ perl/test.pl
@@ -0,0 +1,11 @@
+use Text::Template;
+
+$template = Text::Template->new(
+    type=>'file',
+    source=>'file.tmpl'
+);
+
+$Q::name = 'Fred';
+$Q::age = 45;
+$text = $template->fill_in(package=>'Q');
+print $text;
