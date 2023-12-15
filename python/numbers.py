37bd3213b33d216fe81e1d0aa4c1f243b427ddea
Kyle Eggleston <kyle@dreamsense.com>
2023-12-15 14:26:49 -0700

    added summing of two numbers

---
+++ python/numbers.py
@@ -0,0 +1,5 @@
+print("We're going to sum two numbers!")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num = num1 + num2
+print("The sum of these two numbers is", num)
