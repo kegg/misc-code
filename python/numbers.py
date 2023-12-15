90c803bcdc62c8a9eececd66d58e73cfd1e918d6
Kyle Eggleston <kyle@dreamsense.com>
2023-12-15 15:17:21 -0700

    updated numbers.py added hello world to it

--- python/numbers.py
+++ python/numbers.py
@@ -1,5 +1,10 @@
+import sys
+
 print("We're going to sum two numbers!")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 num = num1 + num2
 print("The sum of these two numbers is", num)
+
+x = "hello world"
+print(x.capitalize())
