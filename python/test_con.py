ef176f44ded10082e2510de3a763f3de5fbf7dc4
Kyle Eggleston <kyle@dreamsense.com>
2023-12-16 21:28:14 -0700

    updated test of converting string to an int

--- python/test_con.py
+++ python/test_con.py
@@ -1,4 +1,7 @@
 # testing a conversion from a string to an int that shouldn't work
-x = 'Fred'
-i = int(x)
+try:
+    x = 'Fred'
+    i = int(x)
+except:
+    print("Nope, no can do.")
 # let's see what happens
