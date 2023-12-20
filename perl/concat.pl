4d3bb90f81a9ed68509a95f694ad46c94dd57957
Kyle Eggleston <kyle@dreamsense.com>
2023-12-19 20:38:33 -0700

    added concat

---
+++ perl/concat.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use 5.10.0; # this is for the say command
+use strict;
+use warnings;
+use utf8;
+
+my $name = 'Fred Jones';
+
+say "Hello my name is ", $name, " and I'm Happy to be here!";
