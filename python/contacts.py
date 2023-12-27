1ffb69d551cb6294622fe2806aef73ef6cf64700
Kyle Eggleston <kyle@dreamsense.com>
2023-12-27 13:58:24 -0700

    Updated Contacts

--- python/contacts.py
+++ python/contacts.py
@@ -1,16 +1,27 @@
 class Contacts:
 
-    def __init__(self, id, age, name):
+    def __init__(self, id, age, name, position):
         self.id = id
         self.age = age
         self.name = name
+        self._position = position
         
     def __str__(self):
-        return "id: %d, age: %d, name: %s" % (self.id, self.age, self.name)
+        return "id: %d, age: %d, name: %s, position: %s" % (self.id, self.age, self.name, self._position)
     
     def checkAge(self):
         if (self.age > 20):
             return "%s is over 20" % (self.name)
         else:
             return "%s is under 20" % (self.name)
-            
+    
+    @property
+    def position(self):
+        return self._position
+    
+    @position.setter
+    def position(self, value):
+        if value == 'Manager' or value == 'Assistant':
+            self._position = value
+        else:
+            print("Position is invalid. No changes made.")
--- python/contacts_use.py
+++ python/contacts_use.py
@@ -1,8 +1,10 @@
 from contacts import Contacts
 
-contact1 = Contacts(1, 23, 'Fred Jones')
-contact2 = Contacts(2, 18, 'Joan Johnson')
+contact1 = Contacts(1, 23, 'Fred Jones', 'Manager')
+contact2 = Contacts(2, 18, 'Joan Johnson', 'Assistant')
 
 print(contact1)
+print(contact1.position)
 print(contact2)
+print(contact2.position)
 print(contact1.checkAge())
