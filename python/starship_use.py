8ba8a02d30e439a152b69d0d8765a931eeaf536d
Kyle Eggleston <kyle@dreamsense.com>
2023-12-29 09:12:00 -0700

    Added two ships

--- python/starship_use.py
+++ python/starship_use.py
@@ -2,10 +2,10 @@ from starship import Starship
 
 enterprise = Starship('U.S.S. Enterprise', 'NCC-1701-D')
 voyager = Starship('U.S.S. Voyager', 'NCC-74656')
+protostar = Starship('U.S.S. Protostar', 'NCC-76884')
+dauntless = Starship('U.S.S. Dauntless', 'NCC-80816')
 
 print(enterprise)
 print(voyager)
-
-enterprise.registry = 'NCC-1701-E'
-
-print(enterprise)
+print(protostar)
+print(dauntless)
