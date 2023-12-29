c4f8f76115e62c03f28925cfa62f7eecfc8ef422
Kyle Eggleston <kyle@dreamsense.com>
2023-12-29 09:16:12 -0700

    added class name to starship class

--- python/starship.py
+++ python/starship.py
@@ -1,8 +1,9 @@
 class Starship:
     
-    def __init__(self, name, registry):
+    def __init__(self, name, registry, className):
         self.name = name
         self.registry = registry
+        self.className = className
     
     def __str__(self):
-        return "Name: %-20s Registry: %s" % (self.name, self.registry)
+        return "Name: %-20s Registry: %-12s Class: %s" % (self.name, self.registry, self.className)
