dbc00d99149002714da936002ca2c07c22f1c00b
Kyle Eggleston <kyle@dreamsense.com>
2023-12-29 09:17:14 -0700

    Corrected Protostar's Registry Number

--- python/starship_use.py
+++ python/starship_use.py
@@ -2,7 +2,7 @@ from starship import Starship
 
 enterprise = Starship('U.S.S. Enterprise', 'NCC-1701-D', 'Galaxy')
 voyager = Starship('U.S.S. Voyager', 'NCC-74656', 'Intrepid')
-protostar = Starship('U.S.S. Protostar', 'NCC-76884', 'Protostar')
+protostar = Starship('U.S.S. Protostar', 'NX-76884', 'Protostar')
 dauntless = Starship('U.S.S. Dauntless', 'NCC-80816', 'Dauntless')
 
 print(enterprise)
