5ed9f82610b3eb0b412a3155b0a152088ae7b0a2
Kyle Eggleston <kyle@dreamsense.com>
2024-02-26 11:02:08 -0700

    updated code to print out yesterday's date

--- java/2024/02/DoIt.java
+++ java/2024/02/DoIt.java
@@ -4,6 +4,7 @@ public class DoIt {
 
     public static void main(String[] args) {
         LocalDateTime ldt = LocalDateTime.now();
-        System.out.println(ldt);
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        System.out.println(yesterday);
     }
 }
