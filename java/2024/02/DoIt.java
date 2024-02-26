769437153dfc2582f9425eeaaeda0a2894d7da7e
Kyle Eggleston <kyle@dreamsense.com>
2024-02-26 08:27:16 -0700

    added class for creating a datetime object

---
+++ java/2024/02/DoIt.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class DoIt {
+
+    public static void main(String[] args) {
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+    }
+}
