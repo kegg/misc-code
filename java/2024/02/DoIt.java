704dd54344795da37392eb8fe264dcfd01378868
Kyle Eggleston <kyle@dreamsense.com>
2024-03-07 20:22:11 -0700

    updated yesterday's date

--- java/2024/02/DoIt.java
+++ java/2024/02/DoIt.java
@@ -3,7 +3,6 @@ import java.time.LocalDateTime;
 public class DoIt {
 
     public static void main(String[] args) {
-        LocalDateTime ldt = LocalDateTime.now();
         LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
         System.out.println(yesterday);
     }
